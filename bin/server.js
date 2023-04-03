946647daf0e3766b639a211d2de2a45685a2dc44
Andreas Rozek <A.Rozek@gmx.de>
2023-04-03 15:34:35 +0200

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -1,35 +1,65 @@
 #!/usr/bin/env node
 
-/**
- * @type {any}
- */
-const WebSocket = require('ws')
-const http = require('http')
-const wss = new WebSocket.Server({ noServer: true })
-const setupWSConnection = require('./utils.js').setupWSConnection
+  const fs        = require('fs')
+  const path      = require('path')
+  const http      = require('http')
+  const https     = require('https')
+  const WebSocket = require('ws')
 
-const host = process.env.HOST || 'localhost'
-const port = process.env.PORT || 1234
+  const setupWSConnection = require('./utils.js').setupWSConnection
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('okay')
-})
+  const host       = process.env.HOST || 'localhost'
+  const port       = process.env.PORT || 1234
+  const CERTFolder = process.env.CERT || ''
 
-wss.on('connection', setupWSConnection)
+  console.clear()
 
-server.on('upgrade', (request, socket, head) => {
-  // You may check auth of request here..
-  // See https://github.com/websockets/ws#client-authentication
-  /**
-   * @param {any} ws
-   */
-  const handleAuth = ws => {
-    wss.emit('connection', ws, request)
+  let KeyFilePath, CERTFilePath
+  if (CERTFolder !== '') {
+    KeyFilePath = path.join(CERTFolder,'privkey.pem')
+    if (! fs.existsSync(KeyFilePath)) {
+      console.error('no key file at "' + KeyFilePath + '"')
+      process.exit(1)
+    }
+
+    CERTFilePath = path.join(CERTFolder,'fullchain.pem')
+    if (! fs.existsSync(CERTFilePath)) {
+      console.error('no cert file at "' + CERTFilePath + '"')
+      process.exit(1)
+    }
   }
-  wss.handleUpgrade(request, socket, head, handleAuth)
-})
 
-server.listen(port, host, () => {
-  console.log(`running at '${host}' on port ${port}`)
-})
+  let server
+  if (CERTFolder !== '') {
+    server = https.createServer({
+      key:  fs.readFileSync(KeyFilePath),
+      cert: fs.readFileSync(CERTFilePath)
+    }, (request, response) => {
+      response.writeHead(200, { 'Content-Type': 'text/plain' })
+      response.end('okay')
+    })
+  } else {
+    server = http.createServer((request, response) => {
+      response.writeHead(200, { 'Content-Type': 'text/plain' })
+      response.end('okay')
+    })
+  }
+
+
+  const wss = new WebSocket.Server({ noServer: true })
+
+  wss.on('connection', setupWSConnection)
+
+  server.on('upgrade', (request, socket, head) => {
+    // You may check auth of request here..
+    // See https://github.com/websockets/ws#client-authentication
+    const handleAuth = ws => {
+      wss.emit('connection', ws, request)
+    }
+
+    wss.handleUpgrade(request, socket, head, handleAuth)
+  })
+
+  server.listen(port, host, () => {
+    console.log(`running at '${host}' on port ${port}`)
+  })
